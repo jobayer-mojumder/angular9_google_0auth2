7f12f92c5d84b6aaae4482dbc02ebf197e6f9f2c
jobayermojumder <jobayer.pro@gmail.com>
2020-05-07 12:33:02 +0200

    api key hide

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDrG31j6DniLtTZ8826kwrMaWVy0Yaoo9s",
-    authDomain: "auth-a8c4c.firebaseapp.com",
-    databaseURL: "https://auth-a8c4c.firebaseio.com",
-    projectId: "auth-a8c4c",
-    storageBucket: "auth-a8c4c.appspot.com",
-    messagingSenderId: "510986270698",
-    appId: "1:510986270698:web:20e6825256b85d63e56493",
-    measurementId: "G-S1EXFPHGME"
+
   }
 };
